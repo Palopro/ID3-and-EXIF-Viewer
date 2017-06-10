92f6eaeac44858390757a9194af008f900a330c8
Vlad <vladharkovenko@gmail.com>
2017-06-10 09:06:47 +0300

    Initial project. Used Library TagLib-Sharp.

---
+++ ID3 and EXIF Viewer/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using TagLib;
+
+namespace ID3_and_EXIF_Viewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TagLib.File file = TagLib.File.Create("01. Take This Life.mp3");
+            String title = file.Tag.Title;
+            String [] artist = file.Tag.Performers;
+            String album = file.Tag.Album;
+            String length = file.Properties.Duration.ToString();
+
+            label1.Text = title;
+            label2.Text = album;
+            label3.Text = length;
+            for(int i=0; i<artist.Length; i++)
+            {
+                label4.Text = artist[i];
+            }
+            
+
+        }
+    }
+}
---
+++ ID3 and EXIF Viewer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ID3_and_EXIF_Viewer
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
